bf08a77e0d540a2647ccc89b3928813499e3a5c0
vinaykontham <148061393+vinaykontham@users.noreply.github.com>
2025-07-12 19:04:42 +0530

    Add .next/static/chunks/src_app_create-infra_page_tsx_8104bf96._.js 1752327282014

---
+++ .next/static/chunks/src_app_create-infra_page_tsx_8104bf96._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@monaco-editor_react_dist_index_mjs_2235a6a0._.js",
+  "static/chunks/src_10790ef5._.js",
+  "static/chunks/node_modules_cdbaeae8._.js"
+],
+    source: "dynamic"
+});
